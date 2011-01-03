9ec046c235380001bbd9b43b19e7d1e40a0235b5
Stephen Celis <stephen@stephencelis.com>
2011-01-02 23:44:49 -0600

    Update Gemfile for latest Bundler.

--- Gemfile
+++ Gemfile
@@ -1,2 +1,3 @@
+source :rubygems
 gem "activesupport"
 gem "rake"
--- miniskirt.rb
+++ miniskirt.rb
@@ -1,4 +1,5 @@
-require "active_support"
+require 'active_support/inflector'
+require 'active_support/core_ext/hash'
 # Factory girl, relaxed.
 #
 #   Factory.define :user do |f|
